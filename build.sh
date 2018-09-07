488ffd024a2096f3b27e64e76a18f45736486870
Ed Rogers <etfrogers@hotmail.com>
2018-09-07 14:57:37 +0100

    Go back to unmerged files. Mereged ones broke

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source activate YEDDA
-pyinstaller YEDDA.spec
+pyinstaller --onefile --add-data="version.txt;version.txt" YEDDA_Annotator.py
+pyinstaller --onefile --add-data="version.txt;version.txt" extract_tagged_text.py
 mv dist/extract_tagged_text.exe dist/extract_tagged_text_${1}.exe
 mv dist/YEDDA_Annotator.exe dist/YEDDA_Annotator_${1}.exe
