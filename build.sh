ddc5f1c5ddc395ed5483ed3bf879d682bf893c7a
Ed Rogers <etfrogers@hotmail.com>
2018-09-07 14:06:44 +0100

    Bugfix in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-activate YEDDA
+source activate YEDDA
 pyinstaller --onefile extract_tagged_text.py
 pyinstaller --onefile YEDDA_Annotator.py
 mv dist/extract_tagged_text.exe dist/extract_tagged_text_${1}.exe
