94a7dfa723104cd996f7d0ee2081b3360f1fb252
Ed Rogers <etfrogers@hotmail.com>
2018-09-07 14:38:31 +0100

    Optimise build process - merge files and move to using spec

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 source activate YEDDA
-pyinstaller --onefile extract_tagged_text.py
-pyinstaller --onefile YEDDA_Annotator.py
+pyinstaller YEDDA.spec
 mv dist/extract_tagged_text.exe dist/extract_tagged_text_${1}.exe
 mv dist/YEDDA_Annotator.exe dist/YEDDA_Annotator_${1}.exe
